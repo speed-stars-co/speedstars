0891e7378a50e413cce7683de75716605ddc3b7c
Speed Stars <speedstarsgame.co@gmail.com>
2025-06-17 09:49:15 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Wacky Nursery!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
